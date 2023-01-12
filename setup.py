7786a083ba71f8a30bc636b120f8900f4b2a5993
Zakarya <65135186+Jouhafa@users.noreply.github.com>
2023-01-12 11:45:05 +0100

    Add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project represents an example of standard data science project',
+    author='Zakarya Jouhafa',
+    license='MIT',
+)
